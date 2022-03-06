2ba83cc94eef8ea73a5b3f4d137cb137c3ac3231
antoniocardos <antoniojgcardoso1@gmail.com>
2022-03-06 18:06:36 +0000

    git_sheets_commands

---
+++ git_commands.ps1
@@ -0,0 +1,15 @@
+#add file to queue
+#git add <file>
+
+#see files on queue
+git status
+
+#Clone remote repository
+git clone username@host:/path/to/repository
+
+#commit files
+git commit –m “Message to go with the commit here”
+
+#if push is requesting username and password
+git remote set-url origin git remote set-url origin git@github.com:antoniocardos/AzureTS_Scripts.git
+git remote -v
